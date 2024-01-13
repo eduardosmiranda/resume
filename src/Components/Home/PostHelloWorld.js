aaccae4470a18cee3f9a80122afd9e09cf346b41
Eduardo Miranda <eduardosilvamiranda1910@gmail.com>
2024-01-13 17:32:43 +0000

    updated MyPhoto.js name

--- src/Components/Home/PostHelloWorld.js
+++ src/Components/Home/PostHelloWorld.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Title from "./Title";
-import MyPhoto from "./MyPhoto";
+import MyPhoto from "./myPhoto";
 import CodeJoke from "./CodeJoke";
 
 function PostHelloWorld () {
