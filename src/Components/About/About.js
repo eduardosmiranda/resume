3a2e1d56a620ef2480443180d20620bd73c4a052
Eduardo Miranda <eduardosilvamiranda1910@gmail.com>
2024-01-13 17:34:08 +0000

    updates Saturday

---
+++ src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuIcon from "../Home/MenuIcon";
+
+function About () {
+    return <div className="container">
+        <MenuIcon/>
+        <h1 className="aboutH1">
+            About Me
+        </h1>
+    </div>
+}
+
+export default About;
